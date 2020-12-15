2b64577fcb4f72422a9221d88a849fe032739620
v.tsarev <v.tsarev@g.nsu.ru>
2020-12-15 20:08:06 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -7,7 +7,7 @@ int BMA(char* Q, int qlen, char* S, int* D, int spos) {
 	for (int i = qlen - 1; i >= 0; i--) {
 		printf("%d ", spos - qlen + i + 1);
 		if (Q[i] != S[i]) {
-			return D[S[qlen - 1]];
+			return D[(int)(S[qlen - 1])];
 		}
 	}
 	return qlen;
@@ -28,6 +28,7 @@ int SfBMA(char* Q, int qlen, char* S, int* D, int add, int spos) {
 	//now the algorithm + recursion
 	int newadd = BMA(Q, qlen, S, D, spos);
 	SfBMA(Q, qlen, S, D, newadd, spos);
+	return 0;
 }
 
 
@@ -46,6 +47,7 @@ int main() {
 		D[(int)(Q[i])] = qlen - i - 1;
 	} // TAI func
 	char t = getchar();
+	if (t == EOF) return 0;
 
 	// BMA is rec
 	SfBMA(Q, qlen, S, D, qlen, 0);// template - size of template - current string (0) - alphabet - how much to add - current position
