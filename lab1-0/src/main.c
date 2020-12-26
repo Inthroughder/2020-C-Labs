b8b359275c04e3fdb81a227e8b136fcda2b0b160
v.tsarev <v.tsarev@g.nsu.ru>
2020-12-26 15:41:24 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,55 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define BUFFER 409600
 
 #include <stdio.h>
 #include <string.h>
 
-int BMA(char* Q, int qlen, char* S, int* D, int spos) {
-	for (int i = qlen - 1; i >= 0; i--) {
-		printf("%d ", spos - qlen + i + 1);
-		if (Q[i] != S[i]) {
-			return D[(int)(S[qlen - 1])];
-		}
-	}
-	return qlen;
-}
-
-int SfBMA(char* Q, int qlen, char* S, int* D, int add, int spos) {
-	for (int i = 0; i < (qlen - add); i++) {
-		S[i] = S[i + add];
-	}
-	for (int i = qlen - add; i < qlen; i++) {
-		char c = getchar();
-		if (c == EOF) return 0;
-		S[i] = c;
-	}
-	spos = spos + add;
-	return spos;
-}
-
-
-int main() {
-	char Q[17];
-	int D[257];
-	char S[17];
-	int h = scanf("%16[^\n]s", Q);
-	if (h == 0) return 0;
-	int qlen = strlen(Q);
-	for (int i = 0; i < 257; i++) {
+void Dfill(int *D, char *Q, int qlen) {
+	for (int i = 0; i < 256; i++) {
 		D[i] = qlen;
 	}
 	for (int i = 0; i < qlen - 1; i++) {
 		D[(int)(Q[i])] = qlen - i - 1;
 	}
+}
 
+int BMA(char *S, char *Q, int qlen, int *D) {
+	int letters_summ = fread(S, sizeof(char), BUFFER, stdin);
+	
+	int gpos = qlen;
+	int spos = qlen - 1;
+	int qpos = qlen - 1;
+	int t = 0;
+	int pm = 0;
+	while (letters_summ - gpos >= 0) {
+		printf("%d ", gpos);
+		if (S[spos] == Q[qpos]) {
+			if (qpos == 0) {
+				qpos = qlen - 1;
+				int kostyl = 2 * qlen - 1;
+				spos = spos + kostyl;
+				gpos = gpos + kostyl;
+				t = 0;
+			} else {
+				gpos--;
+				qpos--;
+				spos--;
+				t++;
+			}
+		} else {
+			int kostyl = D[S[spos + t]] + t;
+			gpos = gpos + kostyl;
+			spos = spos + kostyl;
+			t = 0;
+			qpos = qlen - 1;
+		}
+		
+		if (gpos > letters_summ) {
+			if (feof(stdin))
+				return 0;
+			int smove = qlen - gpos + letters_summ;
+			for (int i = 0; i <= smove; i++) {
+				S[i] = S[BUFFER + pm - smove + i];
+			}
+			spos = qlen - 1;
+			int k = fread(S + smove, sizeof(char), BUFFER, stdin);
+			letters_summ = letters_summ + k;
+			pm = smove;
+		}
+	}
+	return 0;
+}
+
+int main(void) {
+	char Q[17];
+	int D[256];
+	char S[BUFFER + 16];
+
+	scanf("%16[^\n]s", Q);
+
+	int qlen = strlen(Q);
+
+	Dfill(D, Q, qlen);
 
 	int t = getchar();
-	if (t == EOF) return 0;
-	int add = qlen;
-	int spos = 0;
-	while (1) {
-		spos = SfBMA(Q, qlen, S, D, add, spos);
-		if (spos == 0) return 0;
-		add = BMA(Q, qlen, S, D, spos);
-	}
+
+	BMA(S, Q, qlen, D);
+
 	return 0;
 }
