89f1b73a70337af76ad034b302b2691a64bc336d
v.tsarev <v.tsarev@g.nsu.ru>
2020-12-26 19:52:56 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -1,5 +1,24 @@
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER 409600
+
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
 
 int main(void) {
-    return EXIT_SUCCESS;
+	char Q[17];
+	char S[BUFFER + 16];
+
+	if (scanf("%16[^\n]s", Q) == 0)
+		return 0;
+
+	int qlen = strlen(Q);
+
+	int t = getchar();
+	if (t == 0)
+		return 0;
+
+	RKA(S, Q, qlen, hashf(Q, qlen));
+
+	return 0;
 }
