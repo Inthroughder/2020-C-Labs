0276108af88f2eef70732d4ff3bdff46ad44e253
v.tsarev <v.tsarev@g.nsu.ru>
2020-12-26 16:12:53 +0700

    Update main.c

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void sorter(int* A, int begin, int end) {
-	int l = begin, r = end;
-	int v = A[l + (r - l) / 2];
-	while (l <= r) {
-		while (A[l] < v) l++;
-		while (A[r] > v) r--;
-		if (l <= r) {
-			int t = A[l];
-			A[l] = A[r];
-			A[r] = t;
-			l++;
-			r--;
-		}
-	}
-	if (begin < r) sorter(A, begin, r);
-	if (l < end) sorter(A, l, end);
-}
-
 int main() {
 	int n, plen, res, bruh;
 	char S[12];
