197636655db1ac9e9b0f9bf626a686a644dd73a4
v.tsarev <v.tsarev@g.nsu.ru>
2021-02-23 13:54:14 +0700

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
 
 int AD(int* A, int l, int r) {
-	int res = (l + r) / 2;
-	int k = r - l;
-	for (int i = 0; i < k; i++) {
-		if (A[l] <= A[res]) {
+	int p = A[(l + r) / 2];
+	while (r >= l){
+		while (A[l] < p) l++;
+		while (A[r] > p) r--;
+		if (r > l) {
+			int t = A[l];
+			A[l] = A[r];
+			A[r] = t;
 			l++;
-			continue;
-		}
-		if (A[r] >= A[res]) {
 			r--;
-			continue;
+		} else {
+			break;
 		}
-		int t = A[r];
-		A[r] = A[l];
-		A[l] = t;
-		r--;
-		l++;
-		i++;
 	}
-
-	int t = A[r];
-	A[r] = A[res];
-	A[res] = t;
-
-	return l;
+	return r;
 }
 
 int QS(int* A, int l, int r) {
-	while (l < r) {
+	if (l < r) {
 		int m = AD(A, l, r);
-		QS(A, l, m - 1);
-		l = m + 1;
+		QS(A, l, m);
+		QS(A, m + 1, r);
 	}
 	return 0;
 }
 
-int main() {
+int main(void) {
 	int n;
-	int A[2000000];
-	int t = scanf("%d", &n);
+	int A[200];
+	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
-		if (t > 0) t = scanf("%d", &A[i]);
+		scanf("%d", &A[i]);
 	}
 	
 	QS(A, 0, n - 1);
