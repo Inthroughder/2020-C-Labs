1e2f138d0b051ab73400f5fdce4595963a62c297
v.tsarev <v.tsarev@g.nsu.ru>
2021-02-23 14:09:38 +0700

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
+#include "header.h"
 
-int AD(int* A, int l, int r) {
-	int p = A[(l + r) / 2];
-	while (r >= l){
-		while (A[l] < p) l++;
-		while (A[r] > p) r--;
-		if (r > l) {
-			int t = A[l];
-			A[l] = A[r];
-			A[r] = t;
-			l++;
-			r--;
-		} else {
-			break;
-		}
-	}
-	return r;
-}
-
-int QS(int* A, int l, int r) {
-	while (l < r) {
-		int m = AD(A, l, r);
-		QS(A, l, m);
-		l = m + 1;
-	}
-	return 0;
-}
+#define BUFFER 2000000
 
 int main(void) {
 	int n;
-	int A[2000000];
+	int A[BUFFER];
 
 	if (scanf("%d", &n) != 1) {
 		return -1;
